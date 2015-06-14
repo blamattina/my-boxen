5b78f87e1326fb55a0a7c13d19fce6a074ef7d89
Brian LaMattina <brianlamattina@gmail.com>
2015-06-14 19:49:31 -0400

    Add dotfiles

--- modules/people/manifests/blamattina.pp
+++ modules/people/manifests/blamattina.pp
@@ -2,4 +2,17 @@ class people::blamattina {
   include chrome
   include spectacle
   include dropbox
+  include zsh
+  include tmux
+
+  $home = "/Users/brian"
+  $dotfiles = "${home}/dotfiles"
+  $dotfilesLocal = "${home}/dotfiles-local"
+
+  repository {
+    $dotfiles: source => 'thoughtbot/dotfiles'
+  }
+  repository {
+    $dotfilesLocal: source => 'blamattina/dotfiles'
+  }
 }
