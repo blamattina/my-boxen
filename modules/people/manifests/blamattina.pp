a3e5f90867c47515dcb6bf8dceb39610182bc0fa
Brian LaMattina <brianlamattina@gmail.com>
2015-06-18 23:53:58 -0400

    add osx settings

--- modules/people/manifests/blamattina.pp
+++ modules/people/manifests/blamattina.pp
@@ -10,10 +10,33 @@ class people::blamattina {
   $dotfiles = "${home}/dotfiles"
   $dotfilesLocal = "${home}/dotfiles-local"
 
+  # Dotfiles
   repository {
     $dotfiles: source => 'thoughtbot/dotfiles'
   }
   repository {
     $dotfilesLocal: source => 'blamattina/dotfiles'
   }
+
+  # Configuration management
+  homebrew::tap { 'thoughtbot/formulae': }
+  package { 'rcm': ensure => present }
+
+  # Text Editor
+  package {
+    'vim': ensure => present, install_options => [ '--override-system-vi' ]
+  }
+
+  # Dock Settings
+  include osx::dock::autohide
+  include osx::dock::clear_dock
+  class { 'osx::dock::position':
+    position => 'left'
+  }
+
+  # Screen Zoom
+  include osx::universal_access::ctrl_mod_zoom
+  include osx::universal_access::enable_scrollwheel_zoom
+
+
 }
