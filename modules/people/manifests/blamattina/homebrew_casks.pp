ab25e42fb181349579837a8455295007c208de37
Brian LaMattina <brianlamattina@gmail.com>
2016-02-15 16:27:45 -0500

    elm: Add elm
    Adds a sudo workaround:
    https://github.com/boxen/puppet-brewcask/issues/22#issuecomment-150398085

--- modules/people/manifests/blamattina/homebrew_casks.pp
+++ modules/people/manifests/blamattina/homebrew_casks.pp
@@ -1,9 +1,44 @@
 class people::blamattina::homebrew_casks {
   include brewcask
 
-  # casks
-  package { 'transporter-desktop': provider => 'brewcask' }
-  package { 'backblaze': provider => 'brewcask' }
-  package { 'aerial': provider => 'brewcask' }
-  package { 'marked': provider => 'brewcask' }
+  # allow installation of homebrew packages that require sudo
+  sudoers { 'installer':
+    users    => $::boxen_user,
+    hosts    => 'ALL',
+    commands => [
+      '(ALL) SETENV:NOPASSWD: /usr/sbin/installer',
+    ],
+    type     => 'user_spec',
+  }
+
+  # Private cloud storage: http://connecteddata.com
+  package { 'transporter-desktop':
+    provider => 'brewcask',
+    require => Homebrew::Tap['caskroom/cask']
+  }
+
+  # Cloud backup: https://www.backblaze.com/
+  package { 'backblaze':
+    provider => 'brewcask',
+    require => Homebrew::Tap['caskroom/cask']
+  }
+
+  # Apple TV screensavers: https://github.com/JohnCoates/Aerial
+  package { 'aerial':
+    provider => 'brewcask',
+    require => Homebrew::Tap['caskroom/cask']
+  }
+
+  # marked markdown preview: http://marked2app.com/
+  package { 'marked':
+    provider => 'brewcask',
+    require => Homebrew::Tap['caskroom/cask']
+  }
+
+  # elm programming language: http://elm-lang.org/
+  package { 'elm-platform':
+    provider => 'brewcask',
+    require => [ Homebrew::Tap['caskroom/cask'], Sudoers['installer'] ]
+  }
+
 }
