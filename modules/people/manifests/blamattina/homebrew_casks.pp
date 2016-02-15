ea72ecbfca9d76b1eb6e19d8da248824a4b4fbc4
Brian LaMattina <brianlamattina@gmail.com>
2016-02-15 17:42:33 -0500

    brewcask: install apps from brew casks

--- modules/people/manifests/blamattina/homebrew_casks.pp
+++ modules/people/manifests/blamattina/homebrew_casks.pp
@@ -1,7 +1,31 @@
 class people::blamattina::homebrew_casks {
   include brewcask
 
-  # allow installation of homebrew packages that require sudo
+  # brew cask installs that do not require special privilages
+
+  $brewcasks = [
+    '1password',           # 1password: https://agilebits.com/onepassword
+    'aerial',              # ATV Screensavers: https://github.com/JohnCoates/Aerial/
+    'backblaze',           # Backblaze: https://www.backblaze.com/
+    'bartender',           # Bartender 2: https://www.macbartender.com/
+    'chromium',            # Chromium: https://www.chromium.org/
+    'dropbox',             # Dropbox: https://www.dropbox.com/
+    'firefox',             # Firefox: https://www.mozilla.org/en-US/firefox/new/
+    'google-chrome',       # Chrome: https://google.com/chrome/
+    'marked',              # Marked 2: http://marked2app.com/
+    'slack',               # Slack: https://slack.com/
+    'spectacle',           # Spectacle: https://www.spectacleapp.com/
+    'superduper',          # SuperDuper: http://www.shirt-pocket.com/SuperDuper/
+    'transporter-desktop', # FileTransporter: http://connecteddata.com/
+  ]
+
+  package { $brewcasks:
+   provider => 'brewcask',
+    require => Homebrew::Tap['caskroom/cask']
+  }
+
+  # installation of homebrew packages that require sudo
+
   sudoers { 'installer':
     users    => $::boxen_user,
     hosts    => 'ALL',
@@ -11,34 +35,13 @@ class people::blamattina::homebrew_casks {
     type     => 'user_spec',
   }
 
-  # Private cloud storage: http://connecteddata.com
-  package { 'transporter-desktop':
-    provider => 'brewcask',
-    require => Homebrew::Tap['caskroom/cask']
-  }
+  $sudo_brewcasks = [
+    'alfred',       # Alfred 2: https://www.alfredapp.com/
+    'elm-platform', # elm programming language: http://elm-lang.org/
+  ]
 
-  # Cloud backup: https://www.backblaze.com/
-  package { 'backblaze':
-    provider => 'brewcask',
-    require => Homebrew::Tap['caskroom/cask']
-  }
-
-  # Apple TV screensavers: https://github.com/JohnCoates/Aerial
-  package { 'aerial':
-    provider => 'brewcask',
-    require => Homebrew::Tap['caskroom/cask']
-  }
-
-  # marked markdown preview: http://marked2app.com/
-  package { 'marked':
-    provider => 'brewcask',
-    require => Homebrew::Tap['caskroom/cask']
-  }
-
-  # elm programming language: http://elm-lang.org/
-  package { 'elm-platform':
+  package { $sudo_brewcasks:
     provider => 'brewcask',
     require => [ Homebrew::Tap['caskroom/cask'], Sudoers['installer'] ]
   }
-
 }
